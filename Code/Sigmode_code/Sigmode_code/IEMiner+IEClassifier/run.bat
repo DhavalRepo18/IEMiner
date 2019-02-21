e611d572cee91dd1c25a1aec275d64be7c039df5
dhaval <pateldha@us.ibm.com>
2019-02-20 21:01:41 -0500

    added code

---
+++ Code/Sigmode_code/Sigmode_code/IEMiner+IEClassifier/run.bat
@@ -0,0 +1,37 @@
+
+IEMiner part_db0.data 0.10 result_db0.data
+IEMiner part_db1.data 0.10 result_db1.data
+IEMiner part_db2.data 0.10 result_db2.data
+IEMiner part_db3.data 0.10 result_db3.data
+IEMiner part_db4.data 0.10 result_db4.data
+IEMiner part_db5.data 0.10 result_db5.data
+IEMiner part_db6.data 0.10 result_db6.data
+IEMiner part_db7.data 0.10 result_db7.data
+IEMiner part_db8.data 0.10 result_db8.data
+IEMiner part_db9.data 0.10 result_db9.data
+
+
+IEMiner part_dc0.data 0.10 result_dc0.data
+IEMiner part_dc1.data 0.10 result_dc1.data
+IEMiner part_dc2.data 0.10 result_dc2.data
+IEMiner part_dc3.data 0.10 result_dc3.data
+IEMiner part_dc4.data 0.10 result_dc4.data
+IEMiner part_dc5.data 0.10 result_dc5.data
+IEMiner part_dc6.data 0.10 result_dc6.data
+IEMiner part_dc7.data 0.10 result_dc7.data
+IEMiner part_dc8.data 0.10 result_dc8.data
+IEMiner part_dc9.data 0.10 result_dc9.data
+
+
+
+
+IEClassifier result_db0.data result_dc0.data part_db0.data part_dc0.data pattern_0.data pat0.data path0.data
+IEClassifier result_db1.data result_dc1.data part_db1.data part_dc1.data pattern_1.data pat1.data path1.data
+IEClassifier result_db2.data result_dc2.data part_db2.data part_dc2.data pattern_2.data pat2.data path2.data
+IEClassifier result_db3.data result_dc3.data part_db3.data part_dc3.data pattern_3.data pat3.data path3.data
+IEClassifier result_db4.data result_dc4.data part_db4.data part_dc4.data pattern_4.data pat4.data path4.data
+IEClassifier result_db5.data result_dc5.data part_db5.data part_dc5.data pattern_5.data pat5.data path5.data
+IEClassifier result_db6.data result_dc6.data part_db6.data part_dc6.data pattern_6.data pat6.data path6.data
+IEClassifier result_db7.data result_dc7.data part_db7.data part_dc7.data pattern_7.data pat7.data path7.data
+IEClassifier result_db8.data result_dc8.data part_db8.data part_dc8.data pattern_8.data pat8.data path8.data
+IEClassifier result_db9.data result_dc9.data part_db9.data part_dc9.data pattern_9.data pat9.data path9.data
---
+++ Code/Sigmode_code/Sigmode_code/readme.txt
@@ -0,0 +1,2 @@
+Executable are created using in Visual C#. 
+You need microsoft visual c# to be installed on machine before you run.  
